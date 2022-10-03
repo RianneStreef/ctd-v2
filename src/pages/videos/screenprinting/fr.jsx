4b5246b8e84d2ccfa657ff2a5fbf9bba2da36df7
Rianne Streef <56265374+RianneStreef@users.noreply.github.com>
2022-10-03 13:15:17 +0200

    no more empty files

---
+++ src/pages/videos/screenprinting/fr.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+import Layout from "../../../components/Layout";
+import Header from "../../../components/Header";
+import Footer from "../../../components/Footer";
+
+import { content } from "../../../content/languages";
+import { Helmet } from "react-helmet";
+
+import "../../../styles/videos.css";
+
+import video from "../../../images/videos/screenprinting-en.mp4";
+
+const en = (props) => {
+  let { language, setLanguage, languageToUse } = props;
+
+  language === "english"
+    ? (languageToUse = content.english)
+    : (languageToUse = content.french);
+  return (
+    <div>
+      <Header
+        language={language}
+        setLanguage={setLanguage}
+        languageToUse={languageToUse}
+      />
+      <div className="instruction-video">
+        <video id="background-video" autoPlay playsInline loop>
+          <source src={video} type="video/mp4" />
+        </video>
+
+        <h2>Screen printing instruction video</h2>
+      </div>
+      <Footer
+        language={language}
+        setLanguage={setLanguage}
+        languageToUse={languageToUse}
+      />
+    </div>
+  );
+};
+
+en.Layout = Layout;
+export default en;
