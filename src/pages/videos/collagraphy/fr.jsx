9928fbb5fe13aa935ee6a30470892bccc34bc409
Rianne Streef <56265374+RianneStreef@users.noreply.github.com>
2022-11-15 18:52:16 +0100

    Collagraphy added

---
+++ src/pages/videos/collagraphy/fr.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+
+import { graphql } from "gatsby";
+
+import Layout from "../../../components/Layout";
+import Header from "../../../components/Header";
+import Footer from "../../../components/Footer";
+
+import { content } from "../../../content/languages";
+import { Helmet } from "react-helmet";
+
+import "../../../styles/videos.css";
+
+const en = (props) => {
+  let { language, setLanguage, languageToUse } = props;
+
+  language === "english"
+    ? (languageToUse = content.english)
+    : (languageToUse = content.french);
+
+  const { data } = props;
+
+  const video = data.allContentfulInstructionVideo.nodes[0].video.file.url;
+
+  return (
+    <div>
+      <Helmet>
+        <title>Claire Turner Design</title>
+        <meta name="robots" content="noindex" />
+      </Helmet>
+      <Header
+        language={language}
+        setLanguage={setLanguage}
+        languageToUse={languageToUse}
+      />
+      <div className="instruction-video">
+        <video id="background-video" playsInline loop controls>
+          <source src={video} type="video/mp4" />
+        </video>
+        <h2>Video d'instruction Collagraphy</h2>
+      </div>
+      <Footer
+        language={language}
+        setLanguage={setLanguage}
+        languageToUse={languageToUse}
+      />
+    </div>
+  );
+};
+
+export const coursesQuery = graphql`
+  query videosQuery {
+    allContentfulInstructionVideo(filter: { name: { eq: "collagraphy-fr" } }) {
+      nodes {
+        video {
+          file {
+            url
+          }
+        }
+        name
+      }
+    }
+  }
+`;
+
+en.Layout = Layout;
+export default en;
